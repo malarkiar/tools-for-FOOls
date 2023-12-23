10cb34f8cb664a05c40584ea3bdb115573b1cc19
Tyler Perkes <83236226+malarkiar@users.noreply.github.com>
2023-12-22 18:53:12 -0700

    Update ecs-service.tf

--- terraform/ecs-service.tf
+++ terraform/ecs-service.tf
@@ -1,11 +1,11 @@
 resource "aws_ecs_service" "service" {
-  name            = "my-service"
+  name            = var.ecs_service_name
   cluster         = aws_ecs_cluster.cluster.id
   task_definition = aws_ecs_task_definition.task.arn
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets = ["subnet-xxxxxx", "subnet-yyyyyy"]  // Replace with your VPC subnets
+    subnets = var.subnet_cidr_blocks
   }
 
   desired_count = 1
