10de4ee46d87c2ebc6042c9ecdb88e7245b01854
Tyler Perkes <83236226+malarkiar@users.noreply.github.com>
2023-12-22 18:54:52 -0700

    Update ecs-task-def.tf

--- terraform/ecs-task-def.tf
+++ terraform/ecs-task-def.tf
@@ -1,14 +1,14 @@
 resource "aws_ecs_task_definition" "task" {
-  family                   = "my-task"
+  family                   = var.ecs_task_family
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
+  cpu                      = var.ecs_task_cpu
+  memory                   = var.ecs_task_memory
   execution_role_arn       = aws_iam_role.ecs_execution_role.arn
 
   container_definitions = jsonencode([{
     name  = "my-app",
-    image = "my-app-image-url",  // Replace with your Docker image URL
+    image = var.docker_image_url,
     portMappings = [{
       containerPort = 3000,
       hostPort      = 3000
