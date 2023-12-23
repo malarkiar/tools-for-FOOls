5c2e6dfcd1b4579b20e7c6b919839106ffda7963
Tyler Perkes <83236226+malarkiar@users.noreply.github.com>
2023-12-22 18:47:39 -0700

    Create network.tf

---
+++ terraform/network.tf
@@ -0,0 +1,47 @@
+# Create a new VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+# Create subnet - repeat this block for additional subnets
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"  # Replace with your desired AZ
+
+  tags = {
+    Name = "my-subnet"
+  }
+}
+
+# Create a security group for the ECS tasks
+resource "aws_security_group" "ecs_sg" {
+  name        = "ecs-security-group"
+  description = "Security group for ECS tasks"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Ingress rules as needed, e.g., allow HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ecs-security-group"
+  }
+}
