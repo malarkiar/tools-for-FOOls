74e8100284604d4452793e136c780fa1824939a5
Tyler Perkes <83236226+malarkiar@users.noreply.github.com>
2023-12-22 19:10:22 -0700

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -17,3 +17,20 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy_attachment"
   role       = aws_iam_role.ecs_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+# Make sure your ECR repository resource is defined in your Terraform configuration
+# and aws_ecr_repository.my_repository.arn is accessible here
+
+resource "aws_iam_role_policy" "ecr_policy" {
+  name   = "ecr_policy"
+  role   = aws_iam_role.ecs_execution_role.id
+  policy = data.aws_iam_policy_document.ecr_policy.json
+}
+
+data "aws_iam_policy_document" "ecr_policy" {
+  statement {
+    actions   = ["ecr:GetDownloadUrlForLayer", "ecr:BatchGetImage", "ecr:BatchCheckLayerAvailability"]
+    resources = ["arn:aws:ecr:<region>:<account-id>:repository/my-app-repository"]
+    # Replace <region> and <account-id> with actual values and ensure the repository name matches
+  }
+}
